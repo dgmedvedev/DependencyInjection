922d3e9be22f7d5f9735fb27723258f891e7a3aa
dgmedvedev <dg-medvedev@mail.ru>
2023-08-19 15:07:14 +0300

    Initial commit

--- app/src/main/java/com/demo/dependencyinjection/MainActivity.kt
+++ app/src/main/java/com/demo/dependencyinjection/MainActivity.kt
@@ -2,6 +2,7 @@ package com.demo.dependencyinjection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dependencyinjectionstart.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
