3dbddaca4d4cc90dbddb53642d96ef3ca88fb03b
dgmedvedev <dg-medvedev@mail.ru>
2023-09-21 18:56:08 +0300

    subcomponents complete

---
+++ app/src/main/java/com/demo/dependencyinjection/example2/data/di/ActivityComponent.kt
@@ -0,0 +1,21 @@
+package com.demo.dependencyinjection.example2.data.di
+
+import com.demo.dependencyinjection.MainActivity
+import com.demo.dependencyinjection.MainActivity2
+import dagger.BindsInstance
+import dagger.Subcomponent
+
+@Subcomponent(modules = [ViewModelModule::class])
+interface ActivityComponent {
+
+    fun inject(activity: MainActivity)
+
+    fun inject(activity: MainActivity2)
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(
+            @BindsInstance id: String
+        ): ActivityComponent
+    }
+}
